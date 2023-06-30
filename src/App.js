bd4528e54b57624c1228a412d4f3e9950ea6dcd8
christina-mifsud <christina.mifsud@gmail.com>
2023-06-30 22:27:17 +0100

    Level 1, 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from "./logo.svg";
 import "./App.css";
+import allCountryScores from "./scores.js";
 
 function App() {
+  const sortedCountryScores = allCountryScores.sort((a, b) =>
+    a.name.localeCompare(b.name)
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <h1>High Scores per Country</h1>
+        <table className="table">
+          <tbody>
+            {sortedCountryScores.map((country, index) => {
+              return (
+                <>
+                  <h2 key={index}>HIGH SCORES: {country.name}</h2>
+                  {country.scores.map((score, scoreIndex) => {
+                    return (
+                      <tr key={scoreIndex}>
+                        <td>{score.n}</td>
+                        <td>{score.s}</td>
+                      </tr>
+                    );
+                  })}
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
     </div>
   );
 }
